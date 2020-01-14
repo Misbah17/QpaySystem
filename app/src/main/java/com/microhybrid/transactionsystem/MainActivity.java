ec0a4fbf273b3d291c3d606c7aae85b2b95a0ef8
Misbah17 <57543126+Misbah17@users.noreply.github.com>
2020-01-14 19:57:42 +0500

    Add files via upload
    splashscreen

--- app/src/main/java/com/microhybrid/transactionsystem/MainActivity.java
+++ app/src/main/java/com/microhybrid/transactionsystem/MainActivity.java
@@ -1,21 +1,19 @@
 package com.microhybrid.transactionsystem;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-//////import android.os.Handler;
-import android.widget.ProgressBar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.logging.Handler;
-//import java.util.logging.Handler;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int Splashtimeout = 5000;
-    private Handler handler ;
+    private static int Splashtimeout = 3000;
+    Handler handler;
     ProgressBar PBM;
 
 
@@ -27,23 +25,22 @@ public class MainActivity extends AppCompatActivity {
 
         // PBM=findViewById(R.id.progressmain);
         // PBM.setVisibility(View.VISIBLE);
-        //handler = new Handler();
+        handler=new Handler();
 
-
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//                finish();
-//                // PBM.setVisibility(View.GONE);
-//            }
-//        },Splashtimeout);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Homepage.class);
+                startActivity(intent);
+                finish();
+                // PBM.setVisibility(View.GONE);
+            }
+        },Splashtimeout);
 
     }
 
     public void getstart(View view) {
-        Intent startapp = new Intent(MainActivity.this,LoginActivity.class);
+        Intent startapp = new Intent(MainActivity.this, Homepage.class);
         startActivity(startapp);
     }
 }
